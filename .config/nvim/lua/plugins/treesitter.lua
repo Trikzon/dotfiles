86618ca4eb96c363da6401290c43ddbaeac15e8f
Dion Tryban <dion@tryban.dev>
2025-07-12 19:55:36 -0400

    feat(nvim): add treesitter

---
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,24 @@
+return {
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = {
+				"lua",
+			},
+			sync_install = false,
+			highlight = { enable = true },
+			indent = { enable = true },
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "<Enter>",
+					node_incremental = "<Enter>",
+					scope_incremental = false,
+					node_decremental = "<Backspace>",
+				},
+			},
+		})
+	end
+}
+
